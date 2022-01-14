2d0913d2e5e7542fa4aff4d54b35ce284980b0aa
Phawit Pornwattanakul <me@helloyeew.dev>
2022-01-14 19:06:06 +0700

    Add error exception handle on scroll

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ function adjustHeaderColor(start_index, target_index) {
             let element = document.getElementById("menulinkpage" + i);
             element.classList.add("active");
         }
-        start_element.classList.add("active");
+        try {
+            start_element.classList.add("active");
+        } catch (e) {
+            console.log(e);
+        }
+
     }
     if (target_index !== 0){
         for (let i = 1; i <= 4; i++) {
             let element = document.getElementById("menulinkpage" + i);
             try {
                 element.classList.remove("active");
-            } catch {
-                console.log("error");
+            } catch (e) {
+                console.log(e);
             }
         }
-    }
-    if (target_index !== 0) {
         if (start_element.classList.contains("active")) {
             start_element.classList.remove("active");
         }
